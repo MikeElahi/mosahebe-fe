814f6289c8c11ef8d405b5efeee2a5f8bf565cde
Mike Elahi <mikeelahi6@gmail.com>
2023-10-19 00:48:58 +0200

    fix: take ownership of Harold

--- pages/about.vue
+++ pages/about.vue
@@ -10,7 +10,7 @@
         <img src="~/assets/chicken.png" class="max-h-96 mx-auto my-2 select-none pointer-events-none" />
         <p class="text-jusitfy">
             In the quaint town of Webville, there resided a chicken named Harold who possessed a unique gift:
-            he was a web design virtuoso. It all began when Harold observed his owner, a budding web developer named Emily, working late nights on her computer. Intrigued by the world of web design, he began to understand the intricacies of HTML, CSS, and JavaScript. Harold soon embarked on his own projects, crafting stunning websites that wowed the town. His talents made him a local sensation, and he even won the Web Wizardry Competition, cementing his status as a renowned web designer. Harold's remarkable journey proved that talent could flourish in the most unexpected places, inspiring people everywhere with his feathered flair for web design.
+            he was a web design virtuoso. It all began when Harold observed his owner, a budding web developer named Mike, working late nights on his computer. Intrigued by the world of web design, he began to understand the intricacies of HTML, CSS, and JavaScript. Harold soon embarked on his own projects, crafting stunning websites that wowed the town. His talents made him a local sensation, and he even won the Web Wizardry Competition, cementing his status as a renowned web designer. Harold's remarkable journey proved that talent could flourish in the most unexpected places, inspiring people everywhere with his feathered flair for web design.
         </p>
      </UContainer>
 </template>
